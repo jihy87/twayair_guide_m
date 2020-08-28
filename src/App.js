0fab230de66b3cd13cf8e50fd470af074333f0bd
hyji1 <hyji1@DESKTOP-239DPIM>
2020-08-28 17:01:08 +0900

    티웨이항공 모바일 컴포넌트 가이드  초기환경 세팅

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    // <header/><img src="http://contents-image.twayair.com/mobile/images/common/logo_main_red.png" alt="" />
+    <header>
+      <h1>티웨이항공 모바일 컴포넌트 가이드</h1>
+    </header>
+  );
+}
+
+export default App;
